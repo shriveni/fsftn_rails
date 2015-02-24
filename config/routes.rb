d19369885920a2232a8e86482738d4cc34d00958
leonardocr7 <leonardlaureate@gmail.com>
2015-02-24 15:02:53 +0530

    New Blog Files Pushed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :blogs
+
+  get "home" => "home#home"
+  get "about" => "home#about"
+  get "contact" => "home#conatct"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
